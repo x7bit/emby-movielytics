92c629f454d5b5123e96d4d6fee4519be3d4eb21
Rayco Alonso de la Rosa <raycoalonsodelarosa@gmail.com>
2025-11-26 10:07:19 +0000

    Dynamic lang property in html tag (index.html)

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,22 @@ import "@/styles/reset-dark.scss";
 import { createApp } from "vue";
 import { createI18n } from "vue-i18n";
 
-const availableLocales = ["en", "es"];
-const fallbackLocale = "en";
-
-const detectLocale = (): string => {
-  const navLang = navigator.language || (navigator as any).userLanguage || fallbackLocale;
+const detectLocale = (availableLocales: string[], defaultLocale: string): string => {
+  const navLang = navigator.language || (navigator as any).userLanguage || defaultLocale;
   const lang = navLang.toLowerCase().split("-")[0] as string;
-  return availableLocales.includes(lang) ? lang : fallbackLocale;
+  return availableLocales.includes(lang) ? lang : defaultLocale;
 };
 
-const i18n = createI18n({
-  locale: detectLocale(),
-  fallbackLocale: fallbackLocale,
-  messages: { en, es } as any,
-});
+const availableLocales = ["en", "es"];
+const fallbackLocale = "en";
+const locale = detectLocale(availableLocales, fallbackLocale);
+
+if (locale !== fallbackLocale) {
+  document.documentElement.lang = locale;
+}
+
+const messages: any = { en, es };
+const legacy = false;
+const i18n = createI18n({ locale, fallbackLocale, messages, legacy });
 
 createApp(App).use(i18n).mount("#movielytics");
