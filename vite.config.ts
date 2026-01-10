d6b5cbe05535f6b0da9bd6d32a3322f3a08202ee
Rayco Alonso de la Rosa <raycoalonsodelarosa@gmail.com>
2026-01-10 11:40:00 +0000

    Build manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          chart: ["chart.js"],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
