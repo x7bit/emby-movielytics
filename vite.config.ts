ffede496bacb44b2d870334c901ec0087518cc0e
Rayco Alonso de la Rosa <raycoalonsodelarosa@gmail.com>
2026-08-03 01:03:38 +0100

    Revert "Packages update"
    This reverts commit dfaa73c0550894c840a6388fd38a4ad340b931e9.

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
